012de633b4b4ca008c36eaf5a44873eafab13e7d
Robin Ahn <robin.ahn99@gmail.com>
2025-04-16 10:44:58 +0200

    Füge react-router-dom hinzu und implementiere Routing für die Anwendung

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import Test from "./Test.jsx";
 import "./style.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Main = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/test" element={<Test />} />
+            </Routes>
+        </Router>
+    );
+};
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
-    </StrictMode>
+        <Main />
+    </StrictMode>,
 );
